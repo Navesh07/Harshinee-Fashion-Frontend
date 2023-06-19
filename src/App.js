ea9a4137d94a963a4d8e470514b0aa11972406d8
Navesh07 <navesh07@gmail.com>
2023-06-19 17:10:41 +0400

    change to apiBase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App {
   constructor(){
     this.name = "Harshinee Fashion"
     this.version = "1.0.0"
-    this.apiBase = 'http://localhost:3000'
+    this.apiBase = 'https://harshinee-fashion-backend.onrender.com'
     this.rootEl = document.getElementById("root")
     this.version = "1.0.0"
   }
